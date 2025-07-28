dfe5ba87334b67ff01f4c6c5d6e15535d1d2dff9
StrawberSam <s.roche53@hotmail.com>
2025-07-28 11:54:06 +0200

    ✨ | add gitignore

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Write a type-annotated function add that takes a float a and a float b as
+arguments and returns their sum as a float.
+"""
+def add(a: float, b: float) -> float:
+    """Write a type-annotated function add that takes a float a and a float
+    b as arguments and returns their sum as a float.
+
+    Args:
+        a (float): number
+        b (float): number
+
+    Returns:
+        float: type
+    """
+    result = (a, b)
+    return sum(result)
