e9ae2a6942063a8fa535fde2af54e1b4127a8e7e
StrawberSam <s.roche53@hotmail.com>
2025-07-28 14:33:04 +0200

    ✨ | floor.py

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from math import *
 """
 Write a type-annotated function floor which takes a float
 n as argument and returns the floor of the float.
@@ -16,4 +17,4 @@ def floor(n: float) -> int:
     Returns:
         float: return float
     """
-    return round(n)
+    return floor(n)
