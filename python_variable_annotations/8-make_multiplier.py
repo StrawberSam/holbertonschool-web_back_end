53d0acaf0c794983aeb8bb384990fced6132f47f
StrawberSam <s.roche53@hotmail.com>
2025-07-30 10:37:51 +0200

    📝 🔨 | add docstring

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,8 +1,35 @@
 #!/usr/bin/env python3
+"""Write a type-annotated function make_multiplier that takes a float
+multiplier as argument and returns a function that multiplies a float
+by multiplier.
+
+Returns:
+    _type_: function
+"""
 import typing
 
 def make_multiplier(multiplier: float) -> typing.Callable[[float], float]:
+    """Write a type-annotated function make_multiplier that takes a float
+    multiplier as argument and returns a function that multiplies a float
+    by multiplier.
+
+    Args:
+        multiplier (float): float
+
+    Returns:
+        typing.Callable[[float], float]: fonction qui prend deux float
+    """
     def multiply(value: float) -> float:
+        """Write a type-annotated function make_multiplier that takes a
+        float multiplier as argument and returns a function that multiplies
+        a float by multiplier.
+
+        Args:
+            value (float): float
+
+        Returns:
+            float: float
+        """
         result = multiplier * value
         return result
     return multiply
