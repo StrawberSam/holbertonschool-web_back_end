7e17cee3b4c8d8f83e580e7c24d257a27d4b9eed
StrawberSam <s.roche53@hotmail.com>
2025-07-30 10:38:47 +0200

    🔨 | pycodestyle

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -8,6 +8,7 @@ Returns:
 """
 import typing
 
+
 def make_multiplier(multiplier: float) -> typing.Callable[[float], float]:
     """Write a type-annotated function make_multiplier that takes a float
     multiplier as argument and returns a function that multiplies a float
@@ -19,6 +20,7 @@ def make_multiplier(multiplier: float) -> typing.Callable[[float], float]:
     Returns:
         typing.Callable[[float], float]: fonction qui prend deux float
     """
+
     def multiply(value: float) -> float:
         """Write a type-annotated function make_multiplier that takes a
         float multiplier as argument and returns a function that multiplies
