af9fd978208becbe105e44aaa116abedce25e887
StrawberSam <s.roche53@hotmail.com>
2025-08-01 14:34:19 +0200

    ✨ | task 4

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+"""
+Import wait_random from the previous python file that you’ve written and
+write an async routine called wait_n that takes in 2 int arguments
+(in this order): n and max_delay. You will spawn wait_random n times with
+the specified max_delay.
+
+wait_n should return the list of all the delays (float values). The list of
+the delays should be in ascending order without using sort() because of
+concurrency.
+
+Returns:
+    _type_: list of float
+"""
+import asyncio
+import typing
+
+
+async def task_wait_n(n: int, max_delay: int) -> typing.List[float]:
+    """
+    Import wait_random from the previous python file that you’ve written and
+    write an async routine called wait_n that takes in 2 int arguments
+    (in this order): n and max_delay. You will spawn wait_random n times with
+    the specified max_delay.
+
+    wait_n should return the list of all the delays (float values). The list of
+    the delays should be in ascending order without using sort() because of
+    concurrency.
+
+    Args:
+        n (int): number of time
+        max_delay (int): nuùber
+
+    Returns:
+        typing.List[float]: list of float
+    """
+    wait_random = __import__('0-basic_async_syntax').wait_random
+    list_float: list = []
+
+    for i in range(n):
+        list_float.append(wait_random(max_delay))
+    result = await asyncio.gather(*list_float)
+
+    list_result: list = []
+
+    for i in result:
+        if not list_result:
+            list_result.append(i)
+            continue
+
+        inserted = False
+        for index, element in enumerate(list_result):
+            if i < element:
+                list_result.insert(index, i)
+                inserted = True
+                break
+
+        if not inserted:
+            list_result.append(i)
+
+    return list_result
