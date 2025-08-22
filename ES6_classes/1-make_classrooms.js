ec3469c27c5248e4f3f2bd87fb2a7f70d7416f7e
StrawberSam <s.roche53@hotmail.com>
2025-08-22 09:36:17 +0200

    ✨ | Task 1

---
+++ ES6_classes/1-make_classrooms.js
@@ -0,0 +1,6 @@
+import ClassRoom from './0-classroom.js';
+
+export default function initializeRooms() {
+  // retourne ton tableau de 3 ClassRoom
+  return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
+}
