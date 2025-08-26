e5f84aaab42a86af45abcaf45cc4f86ffc63b0b7
StrawberSam <s.roche53@hotmail.com>
2025-08-26 18:15:22 +0200

    🔨  | Task 5

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,11 +1,12 @@
 export default function createInt8TypedArray(length, position, value){
-  const buffer = new ArrayBuffer(length);
-  const view = new Int8Array(buffer);
 
   if (position < 0 || position >= length) {
     throw new Error("Position outside range");
   }
 
-  view[position] = value;
+  const buffer = new ArrayBuffer(length);
+  const view = new Int8Array(buffer);
+
+  view.setInt8(position, value);
   return buffer;
 }
