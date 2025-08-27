16fbac15329b1704a9c3fa38b148f7c43664801a
StrawberSam <s.roche53@hotmail.com>
2025-08-27 10:08:48 +0200

    🔨  | Task 8

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,10 @@
 export default function cleanSet(set, startString){
   const results = []
+
+  if (startString === '' || typeof startString !== 'string') {
+  return '';
+  }
+
   for (let value of set){
     if (value.startsWith(startString)){
       results.push(value.slice(startString.length))
