06429c7e2d0d0178f099d0ecc3816b271d583677
StrawberSam <s.roche53@hotmail.com>
2025-09-02 10:03:29 +0200

    🔨  | task 1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,12 +1,9 @@
-// Afficher un message au début
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-// Ecouter ce que tape l'utilisateur
 process.stdin.on('data', (data) => {
   process.stdout.write(`Your name is: ${data}`);
 });
 
-// Le message de fermeture
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing');
 });
