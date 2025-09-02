894e67e8bd09b77baa0360cc26b5169536fb967d
StrawberSam <s.roche53@hotmail.com>
2025-09-02 09:42:25 +0200

    ✨ | task 1

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,17 @@
+// Afficher un message au début
+console.log("Welcome to Holberton School, what is your name?");
+
+// Ecouter ce que tape l'utilisateur
+process.stdin.on("data", (data) => {
+  const name = data.toString().trim();
+  console.log(`Your name is: ${name}`);
+});
+
+// Le message de fermeture
+process.stdin.on("end", () => {
+  console.log("This important software is now closing");
+});
+
+// data = ce que l’utilisateur a écrit (sous forme de Buffer).
+// toString() → transforme ce Buffer en texte lisible.
+// trim() → enlève les espaces et le retour à la ligne (\n).
