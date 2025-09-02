f00e325948feb89eff13c9ea2be712b14b4ee4a1
StrawberSam <s.roche53@hotmail.com>
2025-09-02 10:00:30 +0200

    🔨  | task 1 ; add process.stdout L6, L11

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,15 +3,10 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 // Ecouter ce que tape l'utilisateur
 process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  console.log(`Your name is: ${name}`);
+  process.stdout.write(`Your name is: ${data}`);
 });
 
 // Le message de fermeture
 process.stdin.on('end', () => {
-  console.log('This important software is now closing');
+  process.stdout.write('This important software is now closing');
 });
-
-// data = ce que l’utilisateur a écrit (sous forme de Buffer).
-// toString() → transforme ce Buffer en texte lisible.
-// trim() → enlève les espaces et le retour à la ligne (\n).
